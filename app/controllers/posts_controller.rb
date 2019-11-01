bc7fc78050e43fbec152944feed176ef99c1b6d6
Hargeon <hirami14590@gmail.com>
2019-11-01 11:50:17 +0300

    add_pagy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.active_posts
+    @pagy, @posts = pagy(Post.active_posts, items: 7)
   end
 
   def new
